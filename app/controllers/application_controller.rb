7e6bf57685683b813e2cfc1c63d28137f0b954c4
m-irtezahussain <irtezahussain04@gmail.com>
2024-07-08 03:40:25 +0500

    work done on dashboard not functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    def auth
+        @current_user = Admin.find_by(id: session[:admin_id])
+        unless @current_user
+          flash[:alert] = 'You need to log in to access this page.'
+          redirect_to login_path
+        end
+      end
 end
