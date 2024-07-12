1abe6668d85e63a75452a66c1cc6f2ff476e48a6
m-irtezahussain <irtezahussain04@gmail.com>
2024-07-12 18:32:02 +0500

    New UI along with addition in recent files

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "/",  to: "home#index"
+  get "/contact-us", to: "home#contact_us" 
 
   get "login", to: "admin#login"
   post "login", to: "admin#create_session"
