51018df94c729ac1a956e0750e041e34f468bff6
m-irtezahussain <irtezahussain04@gmail.com>
2024-07-17 21:01:51 +0500

    Authentication System Completed! Mailer not included yet

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,11 @@
 class AdminController < ApplicationController
 
     def new
-      @admin = Admin.all
+      @admin = Admin.new
     end
 
     def login
-
+      # @admin = Admin.new
     end
 
     def create
@@ -17,28 +17,47 @@ class AdminController < ApplicationController
         else
           @admin = Admin.new(admin_params)
           if @admin.save
-            flash[:notice] = "Admin successfully created."
+            session[:admin_id] = @admin.id
+            flash[:success] = "Admin successfully created."
             redirect_to dashboard_path
           else
-            flash[:alert] = "Failed to create Admin."
+            flash[:error] = "Failed to create Admin."
             render :new
           end
         end
     end
 
     def create_session
-        user = Admin.authenticate(params[:email], params[:password])
-        if user
-            session[:admin_id] = user.id
-            redirect_to dashboard_path
-          else
-            flash.now[:alert] = 'Invalid email/password combination'
-            render 'new'
-          end
+      user = Admin.find_by(email: params[:email])
+
+      if user.present?
+        if user.authenticate(params[:password])
+          session[:admin_id] = user.id
+          flash[:success] = "Logged in Successfully!"
+          redirect_to dashboard_path
+        else
+          flash[:error] = "Invalid Credentials!"
+          redirect_to login_path
+        end
+
+      else
+        flash[:error] = "Unable to find user. <a href='#{admin_new_path}' class='font-semibold'> Register now!</a>".html_safe
+        redirect_to login_path
+      end     
     end
 
-    def request22
-      
+    def request22 
+    end
+
+    def destroy_session
+      if session[:admin_id].present?
+        session.clear
+        flash[:success] = "Logged out successfully!"
+        redirect_to login_path
+      else
+        flash[:alert] = "You are not logged in!"
+        redirect_to root_path
+      end
     end
 
     private
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,9 +2,8 @@ class Admin < ApplicationRecord
 
     has_secure_password
 
-    validates :name, presence: true, length: {maximum: 50}
+    validates :name, presence: true, length: {maximum: 10, message: "Name is too long!"}
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
     validates :password_digest, presence: true
-    
-    
+
 end
