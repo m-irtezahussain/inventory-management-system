d98e76e25e2f21db879880d07f5f78e4d8f6d387
m-irtezahussain <irtezahussain04@gmail.com>
2024-07-29 17:41:01 +0500

    CRUD completed Apis and in dashboard Delete is not working

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,8 +1,8 @@
 class ProductController < ApplicationController
   before_action :auth
+  skip_before_action :verify_authenticity_token
 
   def add_product
-    # abort(params.inspect)
     @new_product = @current_user.products.new(product_params)
     if @new_product.save
       flash[:success] = "Product Added Successfully!"
@@ -14,6 +14,7 @@ class ProductController < ApplicationController
   end
 
   def destroy
+    abort(params.inspect)
     @product = @current_user.products.find_by(id: params[:id])
     if @product.destroy
       flash[:success] = "#{@product.name} is no longer in your inventory list!"
@@ -24,8 +25,23 @@ class ProductController < ApplicationController
     end
   end
 
+  def update
+    selected_product = @current_user.products.find_by(id: params[:id])
+    if selected_product.update(product_params)
+      flash[:success] = "Record for #{selected_product.name} successfully updated!"
+      redirect_back(fallback_location: dashboard_path)
+    else
+      flash[:error] = "Record could not be updated at this very moment kindly try again later."
+      redirect_back(fallback_location: dashboard_path)
+    end
+  end
+
   private
   def product_params 
     params.permit(:name, :brand, :price, :quantity, :category_id)
+  end
+
+  def update_params
+    params.permit(:name, :brand, :price, :quantity)
   end
 end
